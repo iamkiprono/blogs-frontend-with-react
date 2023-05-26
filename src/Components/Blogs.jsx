f3e21b5795c0137f41c659ba9c79437195b2537a
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-05-26 08:51:15 +0000

    Updates

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -50,18 +50,18 @@ const Blogs = () => {
           : blogs.map((blog) => {
               return (
                 
-                <div key={blog.id} className="blog">
+                <div key={blog._id} className="blog">
                   
                   <img src={blog.image} alt="" />
                   <h3 className="text-xl font-bold">{blog.title}</h3>
                   <p className="text-sm my-4"><i>{formatDistanceToNow(new Date(blog.datecreated), {addSuffix:true})}</i></p>
-                  <Link className="text-sm" to={`/${blog.id}`}>
+                  <Link className="text-sm" to={`/${blog._id}`}>
                     <button className="border p-2 mt-4 ">Read more</button>
                   </Link>
                   {user && !loading ? (
                     <button
                       className="delete-btn"
-                      onClick={() => deletePost(blog.id)}
+                      onClick={() => deletePost(blog._id)}
                     >
                       Delete
                     </button>
